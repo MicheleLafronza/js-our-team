da5020ec141065b28122e7c2c0fbe281b77bd337
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-18 15:46:44 +0200

    feat: add objects array

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// creo array di oggetti
+const teamList = [
+    {
+        "nome": "Wayne Barnett",
+        "role": "Founder & CEO",
+        "photo": "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        "nome": "Angela Caroll",
+        "role": "Chief Editor",
+        "photo": "angela-caroll-chief-editor.jpg"
+    },
+
+    {
+        "nome": "Walter Gordon",
+        "role": "Office Manager",
+        "photo": "walter-gordon-office-manager.jpg"
+    },
+
+    {
+        "nome": "Angela Lopez",
+        "role": "Social Media Manager",
+        "photo": "angela-lopez-social-media-manager.jpg"
+    },
+
+    {
+        "nome": "Scott Estrada",
+        "role": "Developer",
+        "photo": "scott-estrada-developer.jpg"
+    },
+
+    {
+        "nome": "Barbara Ramos",
+        "role": "Graphic Designer",
+        "photo": "barbara-ramos-graphic-designer.jpg"
+    },
+]
+
+console.log(teamList);
